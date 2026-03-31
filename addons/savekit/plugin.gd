60e40b0c2c28ac99643f06643865237f2cb02518
Justin Spahr-Summers <justin@jspahrsummers.com>
2026-03-31 20:07:28 +0100

    Stub SaveManager singleton

--- addons/savekit/plugin.gd
+++ addons/savekit/plugin.gd
@@ -1,21 +1,18 @@
 @tool
 extends EditorPlugin
 
+const SAVE_MANAGER_AUTOLOAD_NAME := "SaveManager"
+const SAVE_MANAGER_PATH := "save_manager.gd"
 
 func _enable_plugin() -> void:
-	# Add autoloads here.
-	pass
-
+	add_autoload_singleton(SAVE_MANAGER_AUTOLOAD_NAME, SAVE_MANAGER_PATH)
 
 func _disable_plugin() -> void:
-	# Remove autoloads here.
-	pass
-
+	remove_autoload_singleton(SAVE_MANAGER_AUTOLOAD_NAME)
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	pass
-
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
---
+++ addons/savekit/save_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@export var saveable_group: StringName = &"saveable"
+@export var save_game_directory: String = "user://save_games/"
