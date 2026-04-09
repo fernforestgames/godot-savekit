2f54bc3c6afa4e538a48ec85c07df1f0fecb43b3
Justin Spahr-Summers <justin@jspahrsummers.com>
2026-04-09 18:02:52 +0100

    Add an example `SaveableResource` to demo

---
+++ demo/box_data.gd
@@ -0,0 +1,6 @@
+class_name BoxData
+extends SaveableResource
+## Tracks interaction data for a draggable box, demonstrating SaveableResource usage.
+
+@export var label: String = ""
+@export var click_count: int = 0
